9a46fcef0bd01a13a634fc7def5e371aef3d9bbf
Smulex <53510075+Smulex@users.noreply.github.com>
2020-05-27 11:50:44 +0200

    Add files via upload

---
+++ 8 Queen/8 Queen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Queen
+{
+    class Program
+    {
+         static Solver solver = new Solver();
+        static void Main(string[] args)
+        {
+
+            Console.Write("This is the solver to the N queen problem!\n\nType the N value (4-10): ");
+
+            try
+            {
+                solver.n = Convert.ToInt32(Console.ReadLine());
+                if (solver.n > 10)
+                {
+                    solver.n = 10;
+                }
+            }
+            catch (Exception)
+            {
+                solver.n = 8;
+            }
+
+            Console.WriteLine(solver.PlaceQueens()); 
+
+            Console.ReadKey();
+        }
+
+        public void printSolution(int[,] gameBoard)
+        {
+            Console.WriteLine("Solution number: {0}", solver.solutionNumber++);
+            for (int i = 0; i < solver.n; i++)
+            {
+                for (int j = 0; j < solver.n; j++)
+                {
+                    Console.Write(" {0}", gameBoard[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 8 Queen/8 Queen/Solver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Queen
+{
+    class Solver
+    {
+        public int n;
+        public int solutionNumber = 1;
+
+        public string PlaceQueens()
+        {
+            int[,] gameBoard = new int[n, n];
+
+            if (AllPlacedCheck(gameBoard, 0) == false)
+            {
+                return "Solution not found.";
+            }
+            return "Solution found.";
+        }
+        private bool PlaceOrNotPlace(int[,] board, int row, int column)
+        {
+            int i, j;
+
+            for (i = 0; i < column; i++)
+            {
+                if (board[row, i] == 1)
+                {
+                    return false;
+                }
+            }
+            for (i = row, j = column; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1)
+                {
+                    return false;
+                }
+            }
+            for (i = row, j = column; j >= 0 && i < n; i++, j--)
+            {
+                if (board[i, j] == 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool AllPlacedCheck(int[,] gameBoard, int column)
+        {
+            Program program = new Program();
+            if (column == n)
+            {
+                program.printSolution(gameBoard);
+                return true;
+            }
+
+            bool result = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (PlaceOrNotPlace(gameBoard, i, column))
+                {
+                    gameBoard[i, column] = 1;
+
+                    result = AllPlacedCheck(gameBoard, column + 1) || result;
+
+                    gameBoard[i, column] = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
